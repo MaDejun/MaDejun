b73e5fde085313ed386dafe69e3fee61f00a912d
MaDejun <1510453254@qq.com>
2017-11-06 14:10:59 +0800

    Site updated: 2017-11-06 14:10:51

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://github.com/MaDejun/MaDejun.github.io/tree/master/min_photos' + data.link[i];
-          var src = 'http://github.com/MaDejun/MaDejun.github.io/tree/master/photos' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
